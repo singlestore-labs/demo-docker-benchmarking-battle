f0e7328dd74da7735d1394eee1c6f7b632848f2c
Yaroslav Demenskyi <ydemenskyi@singlestore.com>
2025-05-02 13:05:52 -0700

    add aws docker configs

--- apps/data-generator/indexes.ts
+++ apps/data-generator/indexes.ts
@@ -4,11 +4,16 @@ import { singlestore } from "@repo/singlestore";
 import { sql } from "drizzle-orm";
 
 const indexQueries = [
+  // support users → accounts join
   `CREATE INDEX user_id_idx ON accounts (user_id);`,
+
+  // support accounts → transactions (account_id_from) join
   `CREATE INDEX account_id_from_idx ON transactions (account_id_from);`,
-  `CREATE INDEX account_id_to_idx ON transactions (account_id_to);`,
-  `CREATE INDEX type_id_idx ON transactions (transaction_type_id);`,
+
+  // support getTransactionsSum filter/join on status
   `CREATE INDEX status_id_idx ON transactions (transaction_status_id);`,
+
+  // support listTopRecipients filter on type+status
   `CREATE INDEX type_status_idx ON transactions (transaction_type_id, transaction_status_id);`,
 ];
 
